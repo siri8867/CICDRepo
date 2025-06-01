2d8684342723e8232b48336496fa8a6293a279b8
siri8867 <emanisriksheeraja@gmail.com>
2025-06-01 16:35:23 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull ksheeru/simple-python-flask-app
+sudo docker pull ksheeru/simple-python-flask-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000  ksheeru/simple-python-flask-app
